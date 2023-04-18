a1534089e9e9fe3920b323d14d48c8d60e544707
jayasupriyasomasani <jayasupriya.somasani@narayanagroup.com>
2023-04-18 23:12:38 +0530

    initial commit

---
+++ lib/colors/colors.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/painting.dart';
+
+class AppColor {
+  static Color homePageBg = const Color(0xFFfbfcff);
+  static Color gradientFirst = const Color(0xff0f17ad);
+  static Color gradientSecond = const Color(0xFF6985e8);
+  static Color homePageTitle = const Color(0xFF302f51);
+  static Color homePageSubTitle = const Color(0xFF414160);
+  static Color homePageDetail = const Color(0xFF6588f4);
+  static Color homePageIcons = const Color(0xFF3b3c5c);
+  static Color homePageContainerTxtSm = const Color(0xFFf4f5fd);
+  static Color homePageContainerTxtBig = const Color(0xFFffffff);
+  static Color homePagePlanColor = const Color(0xFFa2a2b1);
+  static Color homePageTopIconColor = const Color(0xFFb7bce8);
+  static Color homePageTitleColor = const Color(0xFFfefeff);
+}
