d97af5c744e8bd9460120c820ba154800564c0b5
jayasupriyasomasani <jayasupriya.somasani@narayanagroup.com>
2023-04-19 12:50:00 +0530

    json data is loaded using DefaultAssetBundle and home page design is implemented

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 
 import 'colors/colors.dart';
@@ -10,6 +12,20 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+
+  List info=[];
+  _initData() {
+    DefaultAssetBundle.of(context).loadString("json/info.json").then((value) {
+      info= json.decode(value);
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _initData();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -64,12 +80,12 @@ class _HomePageState extends State<HomePage> {
                 Text(
                   "Details",
                   style:
-                      TextStyle(color: AppColor.homePageDetail, fontSize: 16),
+                  TextStyle(color: AppColor.homePageDetail, fontSize: 16),
                 ),
-                SizedBox(
+                const SizedBox(
                   width: 4,
                 ),
-                Icon(
+                const Icon(
                   Icons.arrow_forward,
                   size: 20,
                 )
@@ -80,7 +96,10 @@ class _HomePageState extends State<HomePage> {
             ),
             Container(
               height: 220,
-              width: MediaQuery.of(context).size.width,
+              width: MediaQuery
+                  .of(context)
+                  .size
+                  .width,
               decoration: BoxDecoration(
                   gradient: LinearGradient(colors: [
                     AppColor.gradientFirst.withOpacity(0.9),
@@ -98,7 +117,8 @@ class _HomePageState extends State<HomePage> {
                         color: AppColor.gradientSecond.withOpacity(0.5))
                   ]),
               child: Container(
-                padding: const EdgeInsets.only(top: 20, left: 20, right: 20,bottom: 20),
+                padding: const EdgeInsets.only(
+                    top: 20, left: 20, right: 20, bottom: 20),
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
@@ -158,13 +178,172 @@ class _HomePageState extends State<HomePage> {
                               Icons.play_circle_fill,
                               color: Colors.white,
                               size: 60,
-                            ))
+                            )),
                       ],
                     )
                   ],
                 ),
               ),
-            )
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            SizedBox(
+              height: 120,
+              width: MediaQuery
+                  .of(context)
+                  .size
+                  .width,
+              child: Stack(
+                children: [
+                  Container(
+                    height:120,
+                    width: MediaQuery
+                        .of(context)
+                        .size
+                        .width,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        image: const DecorationImage(
+                            image: AssetImage('assets/img.png'),
+                            fit: BoxFit.fill),
+                        boxShadow: [
+                          BoxShadow(
+                              offset: Offset(4, 8),
+                              color: AppColor.gradientSecond.withOpacity(0.5),
+                              blurRadius: 20)
+                        ]),
+                  ),
+                  Container(
+                    height: 120,
+                    width: MediaQuery
+                        .of(context)
+                        .size
+                        .width,
+                    margin: EdgeInsets.only(right: 200),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      image: const DecorationImage(
+                        fit: BoxFit.fitHeight,
+                        image: AssetImage('assets/figure.png'),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    height: 100,
+                    width: double.maxFinite,
+                    margin: EdgeInsets.only(left: 150, top: 20),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "You are doing great..!",
+                          style: TextStyle(
+                              fontSize: 18,
+                              color: AppColor.homePageDetail,
+                              fontWeight: FontWeight.bold),
+                        ),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        RichText(
+                            text: TextSpan(
+                                text: "Keep it up \n",
+                                style: TextStyle(
+                                    color: AppColor.homePagePlanColor,
+                                    fontSize: 16),
+                                children: const [
+                                  TextSpan(text: "Stick to your plan")
+                                ]))
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+            SizedBox(height: 20,),
+            Row(children: [
+              Text(
+                "Area of focus",
+                style: TextStyle(
+                    fontSize: 25,
+                    fontWeight: FontWeight.w500,
+                    color: AppColor.homePageTitle),
+              )
+            ]),
+            Expanded(
+                child: MediaQuery.removePadding(
+                  context: context,
+                  removeTop: true,
+                  child: ListView.builder(
+                      itemCount: info.length.toDouble()~/2,
+                      itemBuilder: (_, i) {
+                        return Row(
+                          children: [
+                            Container(
+                              padding: const EdgeInsets.only(bottom: 10),
+                              margin: const EdgeInsets.only(bottom: 15,left: 15,top: 15),
+                              height: 170,
+                              width: (MediaQuery.of(context).size.width-90)/2,
+                              decoration: BoxDecoration(
+                                  color: Colors.white,
+                                  boxShadow: [
+                                    BoxShadow(
+                                        offset: const Offset(5, 5),
+                                        blurRadius: 20,
+                                        color: AppColor.gradientSecond
+                                            .withOpacity(0.1)),
+                                    BoxShadow(
+                                        offset: const Offset(-5, -5),
+                                        blurRadius: 20,
+                                        color: AppColor.gradientSecond
+                                            .withOpacity(0.1))
+                                  ],
+                                  borderRadius: BorderRadius.circular(15),
+                                  image: DecorationImage(
+                                      image: AssetImage(info[2*i]['img']))),
+                              child: Align(
+                                alignment: Alignment.bottomCenter,
+                                child: Text(
+                                  info[2*i]['title'], style: TextStyle(
+                                    fontSize: 20, color: AppColor.homePageDetail),
+                                ),
+                              ),
+                            ),
+                            Container(
+                              margin: const EdgeInsets.only(bottom: 15,left: 20),
+                              padding: const EdgeInsets.only(bottom: 10),
+                              height: 170,
+                              width: (MediaQuery.of(context).size.width-90)/2,
+                              decoration: BoxDecoration(
+                                  color: Colors.white,
+                                  boxShadow: [
+                                    BoxShadow(
+                                        offset: const Offset(5, 5),
+                                        blurRadius: 20,
+                                        color: AppColor.gradientSecond
+                                            .withOpacity(0.1)),
+                                    BoxShadow(
+                                        offset: const Offset(-5, -5),
+                                        blurRadius: 20,
+                                        color: AppColor.gradientSecond
+                                            .withOpacity(0.1))
+                                  ],
+                                  borderRadius: BorderRadius.circular(15),
+                                  image: DecorationImage(
+                                      image: AssetImage(info[2*i+1]['img']))),
+                              child: Align(
+                                alignment: Alignment.bottomCenter,
+                                child: Text(
+                                  info[2*i+1]['title'], style: TextStyle(
+                                    fontSize: 20, color: AppColor.homePageDetail),
+                                ),
+                              ),
+                            ),
+                          ],
+                        );
+                      }),
+                ))
           ],
         ),
       ),
